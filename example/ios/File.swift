70f136591a89b2438618e08fd850dab055f7b614
pausermake <pausermake@126.com>
2022-07-06 14:41:29 +0800

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  LoExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-lo': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
